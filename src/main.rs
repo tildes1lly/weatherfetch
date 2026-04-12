94438a57daa9bcc0465c8d60d141d9e436854856
tildesilly <imnotsillydev@outlook.com>
2026-04-12 00:52:35 -0400

    fix windows color bug because windows is a picky piece of shit

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,13 @@ mod weather;
 use chrono::prelude::*;
 
 fn main() {
+    
+    #[cfg(target_os = "windows")]
+    colored::control::set_virtual_terminal(true).ok();
     let args: Vec<String> = std::env::args().collect();
     let raw_config = args::parse(args);
     let config = raw_config.unwrap_or_else(|| config::gen_config());
-    
+
     let ip_info = location::fetch_ip_info().expect("Failed to fetch IP info");
     let lat = ip_info.latitude;
     let lon = ip_info.longitude;
