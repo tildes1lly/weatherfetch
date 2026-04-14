585a12d90f9cc0bd1aebe6468f9caa88aacc92be
tildesilly <imnotsillydev@outlook.com>
2026-04-13 22:24:35 -0400

    fix typo causing installation script to fail

--- install/install_unix.sh
+++ install/install_unix.sh
@@ -2,5 +2,5 @@
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source ~/.cargo/env
-cargo install --git https://github.com/tildesilly/weatherfetch
+cargo install --git https://github.com/tildes1lly/weatherfetch
 weatherfetch
